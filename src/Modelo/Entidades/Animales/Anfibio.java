52184b2106d744b94d5cbf9fc1dd56c623bcc3b9
serrrrrgio <sergio.posadag@uqvirtual.edu.co>
2025-09-04 22:43:35 -0500

    se crean los builder de pez y anfibio

--- src/Modelo/Entidades/Animales/Anfibio.java
+++ src/Modelo/Entidades/Animales/Anfibio.java
@@ -1,4 +1,59 @@
 package Modelo.Entidades.Animales;
+import Modelo.Interfaces.IAnimal;
 
-public class Anfibio {
+public class Anfibio extends Animal implements IAnimal {
+
+    private boolean esVenenoso;
+    private String habitat;
+
+    private Anfibio (Builder builder){
+
+        super(builder);
+
+        this.esVenenoso = builder.esVenenoso;
+        this.habitat = builder.habitat;
+    }
+
+    public static class Builder extends Animal.Builder<Builder>{
+
+        private boolean esVenenoso;
+        private String habitat;
+
+        public Builder esVenenoso(boolean esVenenoso){
+            this.esVenenoso = esVenenoso;
+            return this;
+        }
+
+        public Builder habitat(String habitat){
+            this.habitat = habitat;
+            return this;
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+
+        @Override
+        public Animal build(){
+            return new Anfibio(this);
+        }
+    }
+
+
+    public boolean isEsVenenoso() {
+        return esVenenoso;
+    }
+
+    public void setEsVenenoso(boolean esVenenoso) {
+        this.esVenenoso = esVenenoso;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
 }
--- src/Modelo/Entidades/Animales/Pez.java
+++ src/Modelo/Entidades/Animales/Pez.java
@@ -1,4 +1,61 @@
 package Modelo.Entidades.Animales;
 
-public class Pez {
+import Modelo.Interfaces.IAnimal;
+
+public class Pez extends Animal implements IAnimal {
+
+    private String tipoAgua;
+    private double tamanoMaximo;
+
+    private Pez(Builder builder){
+
+        super(builder);
+
+        this.tipoAgua = builder.tipoAgua;
+        this.tamanoMaximo = builder.tamanoMaximo;
+    }
+
+
+    public static class Builder extends Animal.Builder<Builder>{
+
+        private String tipoAgua;
+        private double tamanoMaximo;
+
+        public Builder tipoAgua (String tipoAgua){
+            this.tipoAgua = tipoAgua;
+            return this;
+        }
+
+        public Builder tamanoMaximo(double tamanoMaximo){
+            this.tamanoMaximo = tamanoMaximo;
+            return this;
+        }
+
+        @Override
+        protected Pez.Builder self() {
+            return this;
+        }
+
+        @Override
+        public Animal build() {
+            return new Pez(this);
+        }
+    }
+
+
+    public double getTamanoMaximo() {
+        return tamanoMaximo;
+    }
+
+    public void setTamanoMaximo(double tamanoMaximo) {
+        this.tamanoMaximo = tamanoMaximo;
+    }
+
+    public String getTipoAgua() {
+        return tipoAgua;
+    }
+
+    public void setTipoAgua(String tipoAgua) {
+        this.tipoAgua = tipoAgua;
+    }
 }
